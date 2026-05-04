49cee0c44eca5d120601ffa222896059335d6303
Chaitrali Tambe <136223683+Chaitrali-tambe@users.noreply.github.com>
2026-05-04 10:41:59 +0530

    Add files via upload

---
+++ Model/SNS/Comment1.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorWebAppTest.Model.SNS
+{
+    public class Comment1
+    {
+        [JsonPropertyName("postId")]
+        public int postId { get; set; }
+
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("body")]
+        public string? body { get; set; }
+
+        [JsonPropertyName("likes")]
+        public int likes { get; set; }
+
+        [JsonPropertyName("user")]
+        public Comment1User? CommentUser { get; set; }
+
+        public int? userId => CommentUser.userId;
+
+    }
+
+    public class Comment1User
+    {
+        [JsonPropertyName("id")]
+        public int? userId { get; set; }
+
+    }
+
+}
